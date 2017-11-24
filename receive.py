22d11b3d02300f36d093d31353ea949089fe70f0
sreeragh <sreeragh@qburst.com>
2017-11-24 17:06:26 +0530

    msg acknowledged after processing

--- receive.py
+++ receive.py
@@ -1,16 +1,18 @@
 import pika
 #credentials = pika.PlainCredentials('guest', 'guest')
+import time
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
 def callback(ch, method, properties, body):
+    time.sleep(2)
     print(" [x] Received %r" % body)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
+                      queue='hello')
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
