879224a16ede343e586d2677d378f550cb24d270
sreeragh <sreeragh@qburst.com>
2017-11-24 18:05:41 +0530

    made queue and messages durable, set prefetch count for fair  dispatchig

--- receive.py
+++ receive.py
@@ -4,15 +4,18 @@ import time
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='task_queue', durable=True)
 
 def callback(ch, method, properties, body):
     time.sleep(2)
     print(" [x] Received %r" % body)
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
+#only one task dispatched to a consumer at a time
+channel.basic_qos(prefetch_count=1)
+
 channel.basic_consume(callback,
-                      queue='hello')
+                      queue='task_queue')
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
--- send.py
+++ send.py
@@ -5,9 +5,13 @@ import pika
 #connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', credentials=credentials))
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='task_queue', durable=True)
+
 channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
+                      routing_key='task_queue',
+                      body='Hello World!',
+		      properties=pika.BasicProperties(
+                         delivery_mode = 2, # make message persistent
+                      ))
 print(" [x] Sent 'Hello World!'")
 connection.close()
